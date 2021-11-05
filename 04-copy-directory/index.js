c9b02c0128770a141b23400182adeb1e9bb62264
BlackMamba51 <shvyryov200@gmail.com>
2021-11-05 15:24:20 +0300

    feat: add fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToFile = path.join(__dirname, 'files');
+const pathToNewFile = path.join(__dirname,'files-copy') 
+
+
+fs.mkdir(pathToNewFile, {recursive:true}, (err) => {
+    if (err) throw err;
+});
+
+fs.readdir(pathToFile, 'utf-8', (dirError, data) => {
+    if (dirError) throw dirError;
+
+    for(let file of data) {
+
+        let pathFiles = path.join(pathToFile, file);
+
+        fs.copyFile(pathFiles,path.join(pathToNewFile, file),(copyError) => {
+            if (copyError) throw copyError
+        })
+    }
+});
 
